8987df2c856aa2b991016c177bcbc561df62157b
Вадим <kantora_70@mail.ru>
2023-10-27 09:50:50 +0500

    Задание 25.1

--- courses/management/commands/payment.py
+++ courses/management/commands/payment.py
@@ -9,10 +9,10 @@ class Command(BaseCommand):
         Payments.objects.all().delete()
 
         payments_list = [
-            {'user': User.objects.get(pk=3), 'date_payment': '2023-10-05', 'course': Course.objects.get(pk=2), 'amount': "150000", 'method': 'cash'},
-            {'user': User.objects.get(pk=2), 'date_payment': '2022-08-05', 'course': Course.objects.get(pk=3), 'amount': "125000"},
-            {'user': User.objects.get(pk=2), 'date_payment': '2023-07-11', 'lesson': Lesson.objects.get(pk=3), 'amount': "52000", 'method': 'cash'},
-            {'user': User.objects.get(pk=3), 'date_payment': '2023-05-25', 'lesson': Lesson.objects.get(pk=1), 'amount': "25000", 'method': 'cash'}
+            {'user': User.objects.get(pk=3), 'date_payment': '2023-05-01', 'course': Course.objects.get(pk=2), 'amount': "10000", 'method': 'cash'},
+            {'user': User.objects.get(pk=2), 'date_payment': '2023-03-05', 'course': Course.objects.get(pk=3), 'amount': "12000"},
+            {'user': User.objects.get(pk=2), 'date_payment': '2023-09-22', 'lesson': Lesson.objects.get(pk=3), 'amount': "20000", 'method': 'cash'},
+            {'user': User.objects.get(pk=3), 'date_payment': '2023-06-17', 'lesson': Lesson.objects.get(pk=1), 'amount': "15000", 'method': 'cash'}
         ]
 
         payments_for_create = []
--- users/commands/csu.py
+++ users/commands/csu.py
@@ -6,12 +6,12 @@ from users.models import User
 class Command(BaseCommand):
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin1@mail.ru',
-            first_name='Administator',
-            last_name='Administator',
+            email='kantora.mwn@gmail.com',
+            first_name='Вадим',
+            last_name='Машков',
             is_staff=True,
             is_superuser=True,
             is_active=True
         )
-        user.set_password('123qwer456ty')
+        user.set_password('Vdy1970*')
         user.save()
