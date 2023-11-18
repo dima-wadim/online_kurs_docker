3d9e9636f2762d8404acc08585f9e3a9b906b23f
Вадим <kantora_70@mail.ru>
2023-11-18 11:38:30 +0500

    Доработка. Задание 26.2

--- config/settings.py
+++ config/settings.py
@@ -181,7 +181,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 # Настройки для Celery
 CELERY_BEAT_SCHEDULE = {
     'task-name': {
-        'task': 'myapp.tasks.my_task',  # Путь к задаче
+        'task': 'courses.tasks.send_lesson_update',  # Путь к задаче
         'schedule': timedelta(minutes=90),  # Расписание выполнения задачи (например, каждые 90 минут)
     },
 }
